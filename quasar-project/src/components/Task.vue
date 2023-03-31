e26fa89851b10ee3e35318a3fad3d784bc661c8b
Dennis <dennis.arcelopez@gmail.com>
2023-03-31 14:07:06 -0400

    Commit up to class 3

---
+++ quasar-project/src/components/Task.vue
@@ -0,0 +1,14 @@
+<template>
+    <div> <slot></slot> {{ index }}</div>
+    <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+    <button @click="deleteTask(index)"> X </button>
+</template>
+
+<script>    
+    export default {
+        props: ['task', 'index']
+    }
+</script>
+
+<style>
+</style>
--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,129 +1,65 @@
 <template>
-  <!-- <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page> -->
   <q-page padding>
-    <button
-      style="position: absolute; right: 10px;"
-      @click="counter++"> {{ counter }}
-    </button>
-
-    <input 
-      v-model="message" 
-      @keyup="handleKeyup"
-      v-autofocus 
-      :style="errorStyle"
-      ref="messageInput" />
-
-      <!-- 
-        :class="{ 'error' : message.legnth > 22 }"
-        :style="errorStyle"  -->
-
-
-
-    <!-- <button @click="message =''">Clear</button> -->
-    <button @click="clrearMessage">Clear</button>
-
-    <div> {{ message.length }}</div>
-
-    <h5 
-      v-if="message.length>0"
-      class="border-grey">{{ message }}</h5>
-
-    <h6
-      v-else> No message entered</h6>
-
-    <hr>
-
-    <!-- <p>Uppercase message: {{ messageUpperCase() }}</p> -->
-    <p>Uppercase message: {{ getMessageUpperCase }}</p>
+    <ul>
+      <!-- <li v-for="(task, index) in tasks">
+        <div>{{ task.name }} {{ index }}</div>
+        <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+        <button @click="deleteTask(index)"> X </button>
+      </li> -->
+      <task
+        v-for="(task, index) in tasks"
+        :key="task.id"
+        :task="task"
+        :index="index">
+        {{ task.name }}
+      </task>
+    </ul>
+   
   </q-page>
 </template>
 
 <script>
   import { defineComponent } from 'vue'
+  // import Task from 'components/Task.vue'
 
   export default defineComponent({
     name: 'IndexPage',
+    components: {
+      // 'task': Task
+      'task' : require('components/Task.vue').default
+    },
     data() {
       return {
-        message: 'I love Vue JS',
-        counter: 0
-      }
-    },
-    computed: {
-      getMessageUpperCase(){
-        return this.message.toUpperCase()
-      },
-      errorStyle() {
-        if (this.message.length > 22) {
-          return {
-            'color': 'red',
-            'background': 'pink'
+        tasks: [
+          {
+            id: 1,
+            name: 'Go to shop',
+            dueDate: '2023/04/01',
+            dueTime: '18:30'
+          },
+          {
+            id: 2,
+            name: 'Get bananas',
+            dueDate: '2023/04/01',
+            dueTime: '18:30'
+          },
+          {
+            id: 3,
+            name: 'Get apples',
+            dueDate: '2023/04/01',
+            dueTime: '18:30'
           }
-        }
+      ]
       }
     },
     methods: {
-      clrearMessage(){
-        this.message = ""
-      },
-      handleKeyup(e) {
-        console.log(e)
-        if (e.keyCode == 27){
-          this.clrearMessage()
-        } else if (e.keyCode == 13) {
-          this.alertMessage()
-        }
-      },
-      alertMessage() {
-        alert(this.message)
-      },
-      messageUpperCase(){
-        return this.message.toUpperCase()
+      deleteTask(index) {
+        this.tasks.splice(index,1)
       }
-    },
-    directives:{ 
-      autofocus: {
-        inserted(el) {
-          el.focus()
-          console.log('input inserted')
-        }
-      }
-    },
-    beforeCreate(){
-      console.log('beforeCreate')
-    },
-    created(){
-      console.log('created')
-    },
-    beforeMount(){
-      console.log('beforeMount')
-    },
-    mounted(){
-      console.log('mounted')
-      console.log(this.$refs)
-      this.$refs.messageInput.className = 'bg-green'
-    },
-    unmounted() {
-      console.log('unmounted')
     }
   })
 </script>
 
 <style>
-  .border-grey {
-    border: 1px solid grey;
-  }
-  input, button {
-    font-size: 23px;
-  }
-  .error {
-    color:red;
-    background: pink;
-  }
+
 </style>
